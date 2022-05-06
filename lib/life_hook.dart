37b5587296b1ccc0a481fbb8bdd31631990e5808
Anton Malofeev <anton@xsoulspace.dev>
2022-05-07 02:03:19 +0300

    feat: initial release

---
+++ lib/life_hook.dart
@@ -0,0 +1,50 @@
+library life_hook;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+abstract class LifeState {
+  void initState();
+  void dispose();
+  late VoidCallback setState;
+}
+
+class LifeHook<T extends LifeState> extends Hook<T> {
+  const LifeHook({
+    required this.debugLabel,
+    required this.state,
+  });
+
+  final String debugLabel;
+  final T state;
+
+  @override
+  _LifeHookState<T> createState() => _LifeHookState<T>(state: state);
+}
+
+class _LifeHookState<T extends LifeState> extends HookState<T, LifeHook<T>> {
+  _LifeHookState({required final T state}) : _innerState = state;
+  final T _innerState;
+  @override
+  void initHook() {
+    _innerState
+      ..setState = () {
+        setState(() {});
+      }
+      ..initState();
+    super.initHook();
+  }
+
+  @override
+  T build(final BuildContext context) => _innerState;
+
+  @override
+  void dispose() {
+    _innerState
+      ..setState = () {}
+      ..dispose();
+  }
+
+  @override
+  String get debugLabel => hook.debugLabel;
+}
