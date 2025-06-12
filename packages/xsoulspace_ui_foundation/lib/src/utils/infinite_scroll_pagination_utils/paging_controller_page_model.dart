16c5384215fbe543c8d0c4e921af601366841f95
Arenukvern <anton@xsoulspace.dev>
2025-06-12 03:13:47 +0300

    fix: lints

--- packages/xsoulspace_ui_foundation/lib/src/utils/infinite_scroll_pagination_utils/paging_controller_page_model.dart
+++ packages/xsoulspace_ui_foundation/lib/src/utils/infinite_scroll_pagination_utils/paging_controller_page_model.dart
@@ -1,12 +1,6 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'paging_controller_page_model.g.dart';
+import 'package:flutter/foundation.dart';
 
 @immutable
-@JsonSerializable(
-  explicitToJson: true,
-  genericArgumentFactories: true,
-)
 class PagingControllerPageModel<E> {
   const PagingControllerPageModel({
     required this.values,
@@ -16,11 +10,8 @@ class PagingControllerPageModel<E> {
   factory PagingControllerPageModel.fromJson(
     final Map<String, dynamic> json,
     final E Function(Object? json) fromJsonT,
-  ) =>
-      _$PagingControllerPageModelFromJson(json, fromJsonT);
-  Map<String, dynamic> toJson(
-    final Map<String, dynamic> Function(E) toJsonT,
-  ) =>
+  ) => _$PagingControllerPageModelFromJson(json, fromJsonT);
+  Map<String, dynamic> toJson(final Map<String, dynamic> Function(E) toJsonT) =>
       _$PagingControllerPageModelToJson(this, toJsonT);
 
   final List<E> values;
@@ -31,10 +22,27 @@ class PagingControllerPageModel<E> {
     final List<E>? values,
     final int? pagesCount,
     final int? currentPage,
-  }) =>
-      PagingControllerPageModel<E>(
-        values: values ?? this.values,
-        pagesCount: pagesCount ?? this.pagesCount,
-        currentPage: currentPage ?? this.currentPage,
-      );
+  }) => PagingControllerPageModel<E>(
+    values: values ?? this.values,
+    pagesCount: pagesCount ?? this.pagesCount,
+    currentPage: currentPage ?? this.currentPage,
+  );
 }
+
+PagingControllerPageModel<E> _$PagingControllerPageModelFromJson<E>(
+  final Map<String, dynamic> json,
+  final E Function(Object? json) fromJsonE,
+) => PagingControllerPageModel<E>(
+  values: (json['values'] as List<dynamic>).map(fromJsonE).toList(),
+  currentPage: (json['currentPage'] as num).toInt(),
+  pagesCount: (json['pagesCount'] as num).toInt(),
+);
+
+Map<String, dynamic> _$PagingControllerPageModelToJson<E>(
+  final PagingControllerPageModel<E> instance,
+  final Object? Function(E value) toJsonE,
+) => <String, dynamic>{
+  'values': instance.values.map(toJsonE).toList(),
+  'pagesCount': instance.pagesCount,
+  'currentPage': instance.currentPage,
+};
